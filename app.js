0f0360b8efae8287fb2ed5539c5fb10bf1ec44d4
niravg23 <niravgameti08@gmail.com>
2021-08-03 21:09:50 +0530

    form validation

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const nav=document.querySelector('nav')
 const hamburger=document.querySelector('.hamburger')
 const cross=document.querySelector('.cross')
 
+const button = document.querySelector('#form-button')
+const username = document.querySelector('#name')
+const number=document.querySelector('#number')
+const email = document.querySelector('#email')
+const message=document.querySelector('#message')
+
 hamburger.addEventListener('click', function(e){
     nav.classList.toggle('hide')
     hamburger.classList.toggle('hide')
@@ -21,3 +27,30 @@ window.onscroll= function(e){
         hamburger.classList.add('dark')
     }
 }
+
+const validateForm= function(){
+    let k=[];
+    const array=[username, email,number, message]
+    array.forEach(element => {
+        k.push(element.value.length)
+       
+  });
+  return k
+}
+
+button.addEventListener('click', function(e){
+    const k=validateForm();
+  if(k[0]<4){
+      e.preventDefault()
+      username.placeholder='Name must have atleast 3 characters'
+  } 
+})
+
+button.addEventListener('click', function(e){
+    const k=validateForm();
+    if(k[1]<5){
+        e.preventDefault()
+        email.placeholder='Email is not valid'
+    }
+})
+
